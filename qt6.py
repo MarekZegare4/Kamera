d8c487e5f4736d33f924ddf2d604b0a4c8db5c2c
MarekZegare4 <106778416+MarekZegare4@users.noreply.github.com>
2023-11-25 22:21:17 +0100

    dodana zmiana modeli
    można zmieniać modele, nawet w trakcie działania programu

--- qt6.py
+++ qt6.py
@@ -24,58 +24,97 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Kamera")
-        self.setMinimumSize(QSize(400, 300))
-        self.disply_width = int(800)
-        self.display_height = int(600)
+        self.setMinimumSize(QSize(800, 600))
+        self.resize(self.sizeHint())
+        
+        self.central_widget = QWidget()
+        self.layout = QHBoxLayout()
+        self.set_widget = SettingsWidget()
+        self.vid_widget = VideoWidget()
 
-        # Wyświetlanie wideo
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setLayout(self.layout)
+
+        self.layout.addWidget(self.vid_widget)
+        self.layout.addWidget(self.set_widget)
+        
+ 
+class SettingsWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.label = QLabel('Ustawienia')
+
+        combo = QComboBox(self)
+        combo.addItem('n')
+        combo.addItem('s')
+        combo.addItem('m')
+        combo.addItem('l')
+        combo.addItem('x')
+
+        combo.currentIndexChanged.connect(self.set_model)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(combo)
+
+    def set_model(self, index):
+        global model
+        if index == 0:
+            model = YOLO('yolov8n.pt')
+        if index == 1:
+            model = YOLO('yolov8s.pt')
+        if index == 2:
+            model = YOLO('yolov8m.pt')
+        if index == 3:
+            model = YOLO('yolov8l.pt')
+        if index == 4:
+            model = YOLO('yolov8x.pt')
+
+class VideoWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.label = QLabel('Video')
         self.orgvid_label = QLabel(self)
-        self.modvid_label = QLabel(self)
+        self.orgvid_label.resize(self.orgvid_label.sizeHint()/2)
 
-        # Przycisk do włączania/wyłączania modelu
+        self.disply_width = int(1920/2)
+        self.display_height = int(1080/2)
+
+
         model_on = QPushButton()
         model_on.setText("Model toggle")
         model_on.pressed.connect(self.Click)
 
-        # Połoenie elementów w oknie
-        layout = QGridLayout()
-        widget = QWidget()
-        widget.setLayout(layout)
-        layout.addWidget(self.orgvid_label, 1, 1, Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(self.modvid_label, 1, 2, Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(model_on, 2, 1, Qt.AlignmentFlag.AlignCenter)
-        self.setCentralWidget(widget)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.orgvid_label, Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(model_on)
 
-        # Deklaracja wątków
         self.video_thread = VideoThread()
         self.model_thread = ModelThread()
-
-        # Połączenie wyjściowych wideo do wyświeltania
-        self.video_thread.oryg_video.connect(self.update_orgvid)
-        self.model_thread.model_video.connect(self.update_modvid)
-
+    
         self.video_thread.start()
-        self.model_thread.start()
 
-    # Metoda zarządzająca kliknięciem przycisku
+        self.video_thread.oryg_video.connect(self.update_orgvid)
+
     def Click(self):
         global switch
         switch = not switch
         if switch:
-            self.model_thread.model_video.connect(self.update_modvid)
+            self.model_thread.start()
+            self.video_thread.oryg_video.disconnect(self.update_orgvid)
+            self.model_thread.model_video.connect(self.update_orgvid)
         else:
-            self.video_thread.oryg_video.connect(self.update_modvid)
-    
-    # Metody akutalizujące wyświetlany obraz w oknie
+            self.model_thread.stop()
+            self.model_thread.model_video.disconnect(self.update_orgvid)
+            self.video_thread.oryg_video.connect(self.update_orgvid)
+
     @pyqtSlot(np.ndarray)
     def update_orgvid(self, cv_img):
         qt_img = self.convert_cv_qt(cv_img)
         self.orgvid_label.setPixmap(qt_img)
-        
-    @pyqtSlot(np.ndarray)
-    def update_modvid(self, cv_img):
-        qt_img = self.convert_cv_qt(cv_img)
-        self.modvid_label.setPixmap(qt_img)
+        #self.orgvid_label.setScaledContents(True)
     
     # Zamiana wyjścia z opencv na format, który rozumie Qt
     def convert_cv_qt(self, cv_img):
@@ -86,7 +125,9 @@ class MainWindow(QMainWindow):
         convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format.Format_RGB888)
         p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.AspectRatioMode.KeepAspectRatio)
         return QPixmap.fromImage(p)
-    
+
+
+
 # Wątek odpowiedzialny za pobranie klatek wideo ze źródła
 class VideoThread(QThread):
     oryg_video = pyqtSignal(np.ndarray)
@@ -100,17 +141,27 @@ class VideoThread(QThread):
                 frame = cv_img
                 self.oryg_video.emit(cv_img)
 
+   
+
+
 # Wątek opdowiedzialny za przepuszczanie klatek przez model
 class ModelThread(QThread):
     model_video = pyqtSignal(np.ndarray)
+    active = True
     def run(self):
+        self.active = True
         global frame
-        while True:
-            if len(frame) > 0 and switch:
-                results = model.track(frame, persist=True, show_labels=True)
-                annotated_frame = results[0].plot()
-                self.model_video.emit(annotated_frame)
+        while self.active:
+            print('dzialla')
+            if switch:
+                if len(frame) > 0:
+                    results = model.track(frame, hide_labels=False)
+                    annotated_frame = results[0].plot()
+                    self.model_video.emit(annotated_frame)
 
+    def stop(self):
+        self.active = False
+        self.wait()
 
 app = QApplication(sys.argv)
 window = MainWindow()
